4f9c838d0db3e280b4f67f0bcf8216568906d3d6
ugurukku <ugurkarimli1@gmail.com>
2023-07-23 18:25:01 +0400

    added cors

--- src/main/java/az/millisoft/first/security/Config.java
+++ src/main/java/az/millisoft/first/security/Config.java
@@ -8,6 +8,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +26,9 @@ public class Config {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.formLogin(form -> form
+        http.cors()
+                .and()
+                .formLogin(form -> form
                         .loginPage("/login")
                         .defaultSuccessUrl("/admin")
                         .permitAll()
@@ -32,12 +39,24 @@ public class Config {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(HttpMethod.GET,"/","/images/**","/css/**","/js/**","/fonts/**","/images/**").permitAll()
-                        .requestMatchers(HttpMethod.POST,"/reservation").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/", "/images/**", "/css/**", "/js/**", "/fonts/**", "/images/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/reservation").permitAll()
                         .anyRequest()
                         .authenticated()
                 );
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
